c16f5c159bb86a3127f4206bbb3c8c9586f8c082
Rayosxz <jrayoc@unal.edu.co>
2026-07-20 13:08:35 -0500

    Ejercicio 4.4 - Clase Prueba

---
+++ Ejercicio4_4/Prueba.java
@@ -0,0 +1,11 @@
+package Ejercicio4_4;
+
+public class Prueba {
+
+    public static void main(String[] args) {
+
+        Profesor profesor1 = new ProfesorTitular();
+
+        profesor1.imprimir();
+    }
+}
