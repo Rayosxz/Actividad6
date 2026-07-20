8f30ddfd2470995a6c565ccbc4527c0e208c80f6
Rayosxz <jrayoc@unal.edu.co>
2026-07-20 15:09:25 -0500

    Update Gato.java

--- Ejercicio4_7/Gato.java
+++ Ejercicio4_7/Gato.java
@@ -1,1 +1,24 @@
+package Ejercicio4_7;
 
+public class Gato extends Felino {
+
+    @Override
+    public String getSonido() {
+        return "Maullido";
+    }
+
+    @Override
+    public String getAlimentos() {
+        return "Ratones";
+    }
+
+    @Override
+    public String getHabitat() {
+        return "Doméstico";
+    }
+
+    @Override
+    public String getNombreCientifico() {
+        return "Felis silvestris catus";
+    }
+}
