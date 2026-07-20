dead63b78a9f5c621a1e49e2a71dcdb602f966dc
Rayosxz <jrayoc@unal.edu.co>
2026-07-20 13:07:57 -0500

    Ejercicio 4.4 - Clase ProfesorTitular

---
+++ Ejercicio4_4/ProfesorTitular.java
@@ -0,0 +1,9 @@
+package Ejercicio4_4;
+
+public class ProfesorTitular extends Profesor {
+
+    @Override
+    protected void imprimir() {
+        System.out.println("Es un profesor titular.");
+    }
+}
