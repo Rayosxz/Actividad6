6fc695d48208e5e6743fc8c001479edfe04cc4a3
Rayosxz <jrayoc@unal.edu.co>
2026-07-20 15:09:33 -0500

    Update Leon.java

--- Ejercicio4_7/Leon.java
+++ Ejercicio4_7/Leon.java
@@ -1,1 +1,24 @@
+package Ejercicio4_7;
 
+public class Leon extends Felino {
+
+    @Override
+    public String getSonido() {
+        return "Rugido";
+    }
+
+    @Override
+    public String getAlimentos() {
+        return "Carnívoro";
+    }
+
+    @Override
+    public String getHabitat() {
+        return "Praderas";
+    }
+
+    @Override
+    public String getNombreCientifico() {
+        return "Panthera leo";
+    }
+}
