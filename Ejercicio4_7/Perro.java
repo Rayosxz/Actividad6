a0c8196c0dadd6124879d5856bb6072e7b14637a
Rayosxz <jrayoc@unal.edu.co>
2026-07-20 15:09:49 -0500

    Update Perro.java

--- Ejercicio4_7/Perro.java
+++ Ejercicio4_7/Perro.java
@@ -1,1 +1,24 @@
+package Ejercicio4_7;
 
+public class Perro extends Canido {
+
+    @Override
+    public String getSonido() {
+        return "Ladrido";
+    }
+
+    @Override
+    public String getAlimentos() {
+        return "Carnívoro";
+    }
+
+    @Override
+    public String getHabitat() {
+        return "Doméstico";
+    }
+
+    @Override
+    public String getNombreCientifico() {
+        return "Canis lupus familiaris";
+    }
+}
