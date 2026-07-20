df38a11cf41e037d5907023c6ce01a9a0b2fd538
Rayosxz <jrayoc@unal.edu.co>
2026-07-20 15:09:43 -0500

    Update Lobo.java

--- Ejercicio4_7/Lobo.java
+++ Ejercicio4_7/Lobo.java
@@ -1,1 +1,24 @@
+package Ejercicio4_7;
 
+public class Lobo extends Canido {
+
+    @Override
+    public String getSonido() {
+        return "Aullido";
+    }
+
+    @Override
+    public String getAlimentos() {
+        return "Carnívoro";
+    }
+
+    @Override
+    public String getHabitat() {
+        return "Bosque";
+    }
+
+    @Override
+    public String getNombreCientifico() {
+        return "Canis lupus";
+    }
+}
